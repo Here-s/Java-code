ef444fba2ec2e5926100a6636fd3067aadaed2ba
宋杰鹏 <1817811642@qq.com>
2021-10-30 19:42:52 +0800

    ClassesAndObjects

--- 10_29_ClassesAndObjects/src/ClassesAndObjects.java
+++ 10_29_ClassesAndObjects/src/ClassesAndObjects.java
@@ -6,6 +6,9 @@ class Person{
 
     //字段是定义在类的内部 方法的外部  就是成员变量
     //成员变量：普通成员变量  静态成员变量
+    //静态成员变量的访问：类名.静态的成员属性/方法    普通方法的内部不能定义静态的变量
+    //静态方法的内部也不能定义静态的变量   普通方法内可以调用静态方法
+    //静态方法不能调用普通方法 因为静态的方法不依赖于对象，普通方法依赖于对象
 
     //这里的 public 可以写成 private 就被封装起来了 就只能在当前类当中来使用
     //封装之后会更安全   用 private 来封装
@@ -13,11 +16,28 @@ class Person{
     //public String name;//不建议在这里直接赋值
     public int age;
 
-    //被 final 修饰 还在对象当中   被 static 修饰 放在方法区
+
+    //类加载的时候 就调用了代码块  先执行静态代码块 然后是实例代码块
+    {
+        //实例代码块  可以初始化类成员
+        System.out.println("实例代码块");
+    }
+
+    static {
+        //静态代码块  用来初始化静态成员
+        //如果静态成员在下面 那么结果就是最下面的值 一个静态成员的值只与最后一次赋值有关
+        count = 99;
+        System.out.println("静态代码块");
+    }
+
+    //被 final 修饰 还在对象当中   被 static 修饰 放在方法区 不在类当中
+    //静态的不依赖对象
+    //所有文件只存储一份
 
     //这个代码可以跑 但是不知道怎么修改 也不敢动 就是祖传代码
 
-    public static int count;
+    public static int count = 10;//静态成员变量
+    public final int a = 10;//加 final 之后 就不可以修改值了
 
     public void setName(String myName) {
         name = myName;
@@ -53,6 +73,13 @@ class Person{
         System.out.println(name+" 正在吃饭 "+name+" 正在睡觉");
     }
 
+    public static void staticFunc(){
+        System.out.println("static::func()静态方法");
+    }
+
+    //这样就是动态绑定，就能贺当前类绑定在一块了
+    //因为输出这个类名的时候 就直接返回了这里的 toString 方法
+    //编译器 也可以生成 toString 方法
     @Override //打印属性  这样输出 Person 的引用的时候，就可以返回这里的字符串属性了
     public String toString() {
         return "Person{" +
@@ -68,7 +95,7 @@ class Person{
 
     //不带参数的构造方法  不能改成 private 但是类外是不能实例化对象的
     //下面这三个构造方法是重载
-    public Person() {
+    public Person()  {
         this("Lockey");//进来之后 会调用有一个参数的构造方法
         System.out.println("Person() 不带参数的构造方法");
     }
@@ -82,28 +109,63 @@ class Person{
         System.out.println("Person() 带两个参数的构造方法");
     }
 
-    //静态方法里面不要用 this
+    //静态方法里面不要用 this 能不用 static 就不用 static
     //this.data  调用当前对象的属性
     //this.func  调用函数的属性
-    //this()     调用当前函数不带参数的构造方法
+    //this()     调用当前函数不带参数的构造方法  只能放在第一条语句
+    //成员方法也不能调用构造方法
 }
 public class ClassesAndObjects {
 
     public static void main(String[] args) {
+        //匿名对象 就是没有名字的对象
+        new Person().eat();//这就是匿名对象 是一次性的
+        Person person = new Person();
+        person.eat();
+        person.sleep();
+        System.out.println(person);
+    }
+
+    public static void main8(String[] args) {
+        //不实例化对象 也要执行静态代码块  也是执行一次 在类加载的时候 就执行了静态代码块
+        System.out.println(Person.count);
+    }
+
+    public static void main7(String[] args) {
+        {
+            //这里就是本地代码块
+        }
+        //静态代码块只会被执行一次 与有多少对象无关
+        Person person = new Person();
+        Person person1 = new Person();
+
+    }
+
+    public static void main6(String[] args) {
         Person person = new Person();
         Person p1 = new Person("Lockey");
         Person p2 = new Person("Lockey", 18);
     }
 
     //对于类的调用者 只需要看类的调用接口就行了，不用管类内的区别和变化
-    public static void main4(String[] args) {
+    public static void main5(String[] args) {
         Person person = new Person();
         person.setName("Lockey");
         System.out.println(person.getName());
     }
 
+    Person person = new Person();//在当前的类中 当 new 当前类的时候 就会产生这个 person
+
+    public static void main4(String[] args) {
+        //只要写了这一行代码，cla 在栈上 new 了一个对象 ClassesAndObjects 在堆上
+        //有一个字段 person 在对象 ClassesAndObjects 上，所以就会占用一段堆的空间
+        //所以引用不一定在栈上 也可能在堆上
+        ClassesAndObjects cla = new ClassesAndObjects();
+
+    }
+
 //    public static void main3(String[] args) {
-//        Person person = new Person();
+//        Person person = new Person();//打印的是处理过的地址
 //        System.out.println(person);//但是这里拿到的并不是真正的地址 但他是唯一的
 //        //所以也可以当作地址
 //
@@ -111,15 +173,19 @@ public class ClassesAndObjects {
 //    }
 //
 //    //JVM 加载的时候 要从静态的 main 函数进来  JVM 认为是从静态的 main 开始的
+//    //因为如果方法不加 static 的话 就是普通的成员方法，就需要依赖对象
+//    //所以要调用的话 就要 new 一个对象才能调用
+//    //字节码文件 class 就放在方法区
 //    public static void main2(String[] args) {
+//        Person person = null;//这个引用不指向任何的对象
 //        //p2 和 p1 指向同一个对象
 //        Person p1 = new Person();
-//        Person p2 = p1;
+//        Person p2 = p1;//p2 这个引用指向 p1 这个引用所指向的对象
 //
+//        一个引用不能同时指向多个对象
 //        Person p3 = new Person();
 //        p3 = new Person();
-//        p3 = new Person();//p3 不能指向多个对象  只看最后指向的是那个对象
-//
+//        p3 = new Person();//p3 不能指向多个对象  只看指向最后的那个对象
 //    }
 //
 //    public static void main1(String[] args) {
@@ -145,9 +211,15 @@ public class ClassesAndObjects {
 //        System.out.println(p3.age);
 //        System.out.println(p3.name);
 //
+//        //因为是常量，所以每个对象调用的 count 是同一个 count 因为被 static 修饰了
 //        p2.count++;
 //        System.out.println(p2.count);
 //        p3.count++;
+//
+//        //因为 count 不属于对象 所以可以直接用 类名.count
+//        Person.count++;
 //        System.out.println(p3.count);
+//
+//        Person.staticFunc();//可以通过类目直接访问静态方法
 //    }
 }
